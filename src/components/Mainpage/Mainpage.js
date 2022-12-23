669bef4f8a366fd0815246238c0853124fd5030a
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2022-12-23 13:38:12 +0200

    MainPage&Login✅

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -1,37 +1,46 @@
-import Navbar from "../Navbar/Navbar";
-import WORLDCUP from "../images/worldcup.png"
-import './Mainpage.css'
-import { useNavigate} from 'react-router-dom';
+import "./Mainpage.css";
+import { useNavigate } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
+import { SiGnuprivacyguard } from "react-icons/si";
+import { IoFootball } from "react-icons/io5";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Mainpage(){
-    
-    
-    const navigate = useNavigate();
-    const navigateLogin = () => {
-        navigate('/login');
-      };
+const Mainpage = () => {
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    navigate("/login");
+  };
 
-    const navigateSignup = () => {
-        navigate('/signup');
-      };
-    
-    return(
-        <div>
-            <Navbar />
-            <div className="flex-box">
-                <div className="desc">
-                    <h1>Make your reservation easier</h1>
-                    <div>
-                        <button onClick={navigateLogin}>Login</button>
-                        <button onClick={navigateSignup}>signup</button>
-                    </div>
-                </div>
-                <div className="cup">
-                    <img src={WORLDCUP} alt='cup' className="worldcup" />
-                </div>
+  const navigateSignup = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div>
+      <div className="startContainer">
+        <div className="navbar navbar-expand-md navbar-light">
+          <div class="container-fluid">
+            {/* Login */}
+
+            <div className="loginbtn">
+              <FiLogIn onClick={navigateLogin} />
+              <button className="mainpageButton " onClick={navigateLogin}>login</button>
             </div>
+            {/* signup */}
+            <div className="signupbtn">
+              <SiGnuprivacyguard onClick={navigateSignup} />
+              <button className="mainpageButton" onClick={navigateSignup}>signup</button>
+            </div>
+            {/* Matches */}
+            <div className="match">
+              <IoFootball onClick={navigateSignup} />
+              <button className="mainpageButton" onClick={navigateSignup}>match</button>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Mainpage
+export default Mainpage;
